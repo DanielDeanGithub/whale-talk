bfdd9be8a2b7d6640871b9b5eb81ade22409a600
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-20 10:34:33 +0000

    Created start of for loop for input string

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const input = "Hey, I just met you and this is crazy. But here's my number, so c
 const vowels = ['a','e','i','o','u'];
 
 const resultsArray = [];
+
+for (let i = 0; i < input.length; i++) {
+
+};
